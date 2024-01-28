bd6a62d120e183ed8322840813355b3917297c03
BenJD1205 <s.anh2209@gmail.com>
2024-01-28 23:13:11 +0700

    feat: add database and repository

--- catalog_service/src/repository/catalog.repository.ts
+++ catalog_service/src/repository/catalog.repository.ts
@@ -1,26 +1,43 @@
+import { PrismaClient } from "@prisma/client";
 import { ICatalogRepository } from "../interface/catalogRepository.interface";
 import { Product } from "../models/product.model";
 import { productFactory } from "../utils/fixtures";
 
 export class CatalogRepository implements ICatalogRepository {
+    _prismaClient: PrismaClient
+    constructor() {
+        this._prismaClient = new PrismaClient()
+    }
+
     async create(data: Product): Promise<Product> {
-        const product = productFactory.build();
-        return Promise.resolve(product);
+        return this._prismaClient.product.create({
+            data,
+        })
     }
     async update(data: Product): Promise<Product> {
-        const product = productFactory.build();
-        return Promise.resolve(product);
+        return this._prismaClient.product.update({
+            where: { id: data.id },
+            data
+        })
     }
     async delete(id: any) {
-        const product = productFactory.build();
-        return Promise.resolve(product);
+        return this._prismaClient.product.delete({
+            where: { id: id },
+        })
     }
     async find(limit: number, offset: number): Promise<Product[]> {
-        const products = productFactory.buildList(limit);
-        return Promise.resolve(products);
+        return this._prismaClient.product.findMany({
+            take: limit,
+            skip: offset
+        })
     }
     async findOne(id: number): Promise<Product> {
-        const product = productFactory.build();
-        return Promise.resolve(product);
+        const product = this._prismaClient.product.findFirst({
+            where: { id: id },
+        })
+        if (product) {
+            return Promise.resolve(product);
+        }
+        throw new Error("product not found");
     }
 }
